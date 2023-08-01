fe0d8047e84c7f20192eb69f1cc3a1ae60f8040d
Anilsonawanetestor <138239247+AnilSonawane2598@users.noreply.github.com>
2023-08-01 13:17:57 +0530

    Update new_base_class.java

--- src/main/java/Base_package/new_base_class.java
+++ src/main/java/Base_package/new_base_class.java
@@ -1,6 +1,9 @@
 package Base_package;
 
 public class new_base_class {
-gfg
+This branch has conflicts that must be resolved
+Use the web editor or the  to resolve conflicts.
+Conflicting files
+src/main/java/Base_package/new_base_class.java
 }
 }
