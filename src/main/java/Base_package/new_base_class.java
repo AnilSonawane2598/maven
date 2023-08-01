a5a829235f1060a3224a673edc4f7f7d011b6dd3
anil <anilsonawane@gmail.com>
2023-08-01 12:56:33 +0530

    ty

--- src/main/java/Base_package/new_base_class.java
+++ src/main/java/Base_package/new_base_class.java
@@ -1,7 +1,7 @@
 package Base_package;
 
 public class new_base_class {
-driver.manage().window().maximize();
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+	//	driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+	//	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 }
